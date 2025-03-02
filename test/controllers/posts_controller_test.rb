eea4dd902dc516fabb53c3281b145da36e1e541f
SonMyeongJin <smj2802@naver.com>
2025-03-02 13:41:38 +0900

    [Fix] Modify controller for test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,28 +1,36 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @post = posts(:one) # 예시로 @post 픽스처 설정
+  end
+
   test "should get index" do
-    get posts_index_url
+    get posts_url
     assert_response :success
   end
 
-  test "should get show" do
-    get posts_show_url
+  test "should show post" do
+    get post_url(@post)
     assert_response :success
   end
 
-  test "should get create" do
-    get posts_create_url
-    assert_response :success
+  test "should create post" do
+    assert_difference('Post.count') do
+      post posts_url, params: { post: { title: 'New Title', content: 'Some content', category_id: categories(:one).id } }
+    end
+    assert_redirected_to post_url(Post.last) # 생성 후 해당 포스트의 상세 페이지로 리다이렉트되는지 확인
   end
 
-  test "should get update" do
-    get posts_update_url
-    assert_response :success
+  test "should update post" do
+    patch post_url(@post), params: { post: { title: 'Updated Title', content: 'Updated content' } }
+    assert_redirected_to post_url(@post) # 업데이트 후 해당 포스트의 상세 페이지로 리다이렉트되는지 확인
   end
 
-  test "should get destroy" do
-    get posts_destroy_url
-    assert_response :success
+  test "should destroy post" do
+    assert_difference('Post.count', -1) do
+      delete post_url(@post)
+    end
+    assert_redirected_to posts_url # 삭제 후 포스트 목록 페이지로 리다이렉트되는지 확인
   end
 end
