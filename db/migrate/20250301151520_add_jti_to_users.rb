e1f2662352a1035d2722554ebcf3ff9aad30d141
SonMyeongJin <smj2802@naver.com>
2025-03-02 13:35:49 +0900

    [Fix] Modify test user

--- db/migrate/20250301151520_add_jti_to_users.rb
+++ db/migrate/20250301151520_add_jti_to_users.rb
@@ -1,10 +1,10 @@
 class AddJtiToUsers < ActiveRecord::Migration[7.0]
   def change
     unless column_exists?(:users, :jti)
-      add_column :users, :jti, :string
-      User.reset_column_information # 필요한 경우
+      add_column :users, :jti, :string, default: ''
+      User.reset_column_information
       User.find_each { |user| user.update_column(:jti, SecureRandom.uuid) }
-      change_column_null :users, :jti, false
+      change_column :users, :jti, :string, null: false
       add_index :users, :jti, unique: true
     end
   end
