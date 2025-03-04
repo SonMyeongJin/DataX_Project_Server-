adc5fc637c22a9b8aa8a9b65d4055d3da15dae26
SonMyeongJin <smj2802@naver.com>
2025-03-05 00:37:29 +0900

    [Fix] Cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # Vue.js 개발 서버 주소
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options],
       expose: ['Authorization'],
-      credentials: true
+      credentials: false
   end
 end
