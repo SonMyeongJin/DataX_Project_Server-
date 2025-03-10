574a39f0fdec049f34fb49a19771ebd6e3b25c3b
SonMyeongJin <smj2802@naver.com>
2025-03-10 16:34:10 +0900

    [Fix] Cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://54.180.239.200'  # * 대신 특정 출처 지정
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options],
